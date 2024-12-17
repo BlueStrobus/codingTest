c4bfb16eab5d075cce2beda6863d9e0a14507d01
strobus <YoonStrobus@gmail.com>
2024-12-17 10:00:37 +0900

    Update 문자열 다루기 기본.js

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,15 @@
 function solution(s) {
-  var re = /^\d{6}$|^\d{4}$/;
-  return re.test(s);
+    // 문자열 길이 확인
+    if (s.length !== 4 && s.length !== 6) {
+        return false;
+    }
+
+    // 문자열의 각 문자가 숫자인지 확인
+    // 공백 예외처리 추가
+    for (let i = 0; i < s.length; i++) {
+        if (isNaN(Number(s[i])) || s[i] === ' ') {
+            return false;
+        }
+    }
+    return true;
 }
