1662e2cbe2b3675500902f8099294c5d09086229
strobus <YoonStrobus@gmail.com>
2024-12-19 09:26:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 54.78 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,16 +1,7 @@
-process.stdin.setEncoding("utf8");
-process.stdin.on("data", (data) => {
-  const p = data.split(" ");
-  //
-  const n = Number(p[0]),
-    m = Number(p[1]);
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const p = data.split(" ");
+    const n = Number(p[0]), m = Number(p[1]);
 
-  let strN = "";
-  for (let i = 0; i < n; i++) {
-    strN = strN + "*";
-  }
-  for (let i = 0; i < m; i++) {
-    console.log(strN);
-  }
-
+    console.log(('*'.repeat(n) + '\n').repeat(m).trim());
 });
