5ae46f706763cd2a028942d21fa31227713ec998
strobus <YoonStrobus@gmail.com>
2025-01-02 09:48:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.27 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,4 @@
 function solution(s) {
-    let arrAnswer = [];
-    let lastIndexMap = {}; // 문자의 마지막 등장 위치를 저장
-
-    [...s].forEach((char, idx) => {
-        if (lastIndexMap[char] !== undefined) {
-            arrAnswer.push(idx - lastIndexMap[char]); // 현재 인덱스 - 마지막 등장 인덱스
-        } else {
-            arrAnswer.push(-1); // 처음 등장하는 경우
-        }
-        lastIndexMap[char] = idx; // 마지막 등장 위치 갱신
-    });
-
-    console.log(lastIndexMap);
-    return arrAnswer;
+    var answer = [];
+    return answer;
 }
