1e81f57f777983649020a783f249a03f9ef72ab3
strobus <YoonStrobus@gmail.com>
2025-01-14 09:12:45 +0900

    [level 1] Title: 모의고사, Time: 1.78 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,25 +3,11 @@ const guessed2 = [2, 1, 2, 3, 2, 4, 2, 5];
 const guessed3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
 function solution(answers) {
-    let score = [0, 0, 0];
-    answers.forEach((v, i) => {
-        if (v === guessed1[i % guessed1.length]) {
-            score[0] += 1;
-        }
-        if (v === guessed2[i % guessed2.length]) {
-            score[1] += 1;
-        }
-        if (v === guessed3[i % guessed3.length]) {
-            score[2] += 1;
-        }
-    });
-    // 배열에서 최대값 찾기
-    const maxValue = Math.max(...score);
-    // 최대값을 가진 인덱스들을 배열로 반환
-    return score.reduce((indices, value, index) => {
-        if (value === maxValue) {
-            indices.push(index + 1);
-        }
-        return indices;
-    }, []);
+    const patterns = [guessed1, guessed2, guessed3];
+    const scores = patterns.map(pattern => 
+        answers.filter((v, i) => v === pattern[i % pattern.length]).length
+    );
+
+    const maxScore = Math.max(...scores);
+    return scores.map((score, i) => (score === maxScore ? i + 1 : null)).filter(v => v);
 }
