9fd4142282952eb1f4b6f1306f9ea48559b59a34
strobus <YoonStrobus@gmail.com>
2025-02-12 09:29:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,1 @@
-function solution(s) {
-  return s
-    .split(" ")  // 공백 기준으로 단어 분할
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())  // 첫 글자는 대문자, 나머지는 소문자로 변환
-    .join(" ");  // 다시 문자열로 합치기
-}
+const solution = s => s.replace(/\b\w/g, c => c.toUpperCase()).replace(/\B\w/g, c => c.toLowerCase());
