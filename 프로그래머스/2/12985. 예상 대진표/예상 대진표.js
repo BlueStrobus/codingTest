e327828be9399a8fb654daa0a9f87b5dad3facd0
strobus <YoonStrobus@gmail.com>
2025-02-18 09:31:17 +0900

    [level 2] Title: 예상 대진표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,5 @@
 function solution(N, A, B) {
-    let round = 1;
-    
-    while (A !== B) {
-        A = Math.ceil(A / 2);
-        B = Math.ceil(B / 2);
-        round++;
-    }
-    
-    return round - 1;
+    let r = 0;
+    while (A !== B) A = (A + 1) >> 1, B = (B + 1) >> 1, r++;
+    return r;
 }
