765d88b6c7271ee13705bba04c57682de77ffc85
strobus <YoonStrobus@gmail.com>
2025-02-19 09:43:47 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,34 +1,7 @@
-function primeFactorization(n) {
-    let factors = {};
-    let divisor = 2;
-    
-    while (n > 1) {
-        while (n % divisor === 0) {
-            factors[divisor] = (factors[divisor] || 0) + 1;
-            n /= divisor;
-        }
-        divisor++;
-    }
-    
-    return factors;
+function solution(arr) {
+    return arr.reduce((a, b) => (a * b) / gcd(a, b));
 }
 
-const solution = (arr) => {
-    let lcmFactors = {};
-
-    for (let num of arr) {
-        let factors = primeFactorization(num);
-
-        for (let factor in factors) {
-            lcmFactors[factor] = Math.max(lcmFactors[factor] || 0, factors[factor]);
-        }
-    }
-
-    // 최소공배수 계산
-    let lcm = 1;
-    for (let factor in lcmFactors) {
-        lcm *= Math.pow(parseInt(factor), lcmFactors[factor]);
-    }
-
-    return lcm;
-};
+function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+}
