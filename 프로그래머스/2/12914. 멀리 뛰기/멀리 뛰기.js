92777e7643d3f267a38d0999f94062e1ba1f30fc
strobus <YoonStrobus@gmail.com>
2025-02-24 08:32:31 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,10 @@
+const solution = (n) => {
+  const MOD = 1234567;
+  let dp = [0, 1, 2];  // dp[1] = 1, dp[2] = 2
+
+  for (let i = 3; i <= n; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+  }
+
+  return dp[n];
+};
